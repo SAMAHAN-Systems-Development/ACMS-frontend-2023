16f4e6caed0df7be2b020f4147c568ea239ba9a6
Viver Bungag <viverbungag1@gmail.com>
2024-01-09 20:30:40 +0800

    Finished requirements except the UI - The frontend endpoint should be '/payments/accepted' - When 'Select All' button is clicked, it will select all cards - When 'Restore all selected' button is clicked, it will trigger the backend endpoint which is '/payments/restore' and will restore all selected - Clicking the card will select or unselect the payment - The 'Select All' button will change to 'Unselect All' when all cards are selected - When 'Unselect All' button is clicked, all cards will be unselected - Clicking the 'Restore' button will trigger the backend endpoint which is '/payments/restore' and will restore the payment

---
+++ src/stories/components/payments/accepted/Page.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import PaymentsPage from '@/components/payments/PaymentsPage';
+import type Payment from '@/types/Payment';
+
+const meta: Meta<typeof PaymentsPage> = {
+  title: 'payments/accepted/Page',
+  component: PaymentsPage,
+};
+
+export default meta;
+type Story = StoryObj<typeof PaymentsPage>;
+
+const listOfAcceptedPayments: Payment[] = [
+  {
+    id: 'sample-id-1',
+    eventPrice: '100',
+    eventTitle: 'Codes 1',
+    studentName: 'Jay',
+    paymentPhotoUrl: '/placeholderImage.jpg',
+  },
+  {
+    id: 'sample-id-2',
+    eventPrice: '200',
+    eventTitle: 'Codes 2',
+    studentName: 'Inhahnn',
+    paymentPhotoUrl: '/placeholderImage.jpg',
+  },
+];
+
+const restoreButtonAction = (ids: string[]) => {
+  ids;
+};
+
+export const Primary: Story = {
+  render: () => (
+    <PaymentsPage
+      listOfAcceptedPayments={listOfAcceptedPayments}
+      restoreButtonAction={restoreButtonAction}
+    />
+  ),
+};
