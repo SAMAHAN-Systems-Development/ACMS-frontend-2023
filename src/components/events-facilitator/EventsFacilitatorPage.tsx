ee17acd11e5b523a671f668ebf42a714217747b5
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2024-01-12 11:37:56 +0800

    added events facilitator page component

---
+++ src/components/events-facilitator/EventsFacilitatorPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+type Props = {
+
+}
+
+export const EventsFacilitatorPage: React.FC = ({
+
+}: Props) => {
+    return (
+        <section className='flex justify-center items-center'>
+
+        </section>
+    )
+}
+
