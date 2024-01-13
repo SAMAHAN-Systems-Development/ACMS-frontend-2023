7430cdaab2118a844f1504193cd6710653b07f66
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2024-01-13 10:49:09 +0800

    feat: added sample story & configured storybook

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+import React from 'react';
+
+const Button: FC = () => {
+  return <div className="bg-red-500 font-bold">Button</div>;
+};
+
+export default Button;
