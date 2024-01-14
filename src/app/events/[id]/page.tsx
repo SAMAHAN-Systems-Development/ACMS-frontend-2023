d6edc44e1cda5e7b467c26c005d431473c5cbacc
iyabc <juliennepanes@gmail.com>
2024-01-14 19:25:29 +0800

    feat(StudentsTable): created StudentsTable, added search students functionality, refactored fetch calls in EventsPage

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,178 +1,61 @@
-'use client';
+'use server';
 
-import Link from 'next/link';
-import type { FC } from 'react';
 import React from 'react';
+import Link from 'next/link';
 
-type FacilitatorType = {
-  email: string;
-  name: string;
+import PaymentsModal from '@/components/payments/PaymentsModal';
+import Button from '@/components/ui/Button';
+import * as StudentsTable from '@/components/ui/StudentsTable.client';
+
+const backendUrl =
+  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
+
+const getEventData = async (id: string) => {
+  const response = await fetch(`${backendUrl}/event/${id}`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch event data!');
+  }
+
+  return response.json();
 };
 
-type StudentType = {
-  name: string;
-  uuid: string;
-  yearCourse: string;
+const getPaymentsData = async (id: string, paymentType: string) => {
+  const response = await fetch(`${backendUrl}/payment/${paymentType}`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch event payments data!');
+  }
+
+  return response.json();
 };
 
-type ButtonProps = {
-  text: string;
-  onClickHandler?: () => void;
-};
-
-const facilitators: FacilitatorType[] = [
-  {
-    name: 'john',
-    email: 'john@addu.edu.ph',
-  },
-  {
-    name: 'jane',
-    email: 'jane@addu.edu.ph',
-  },
-  {
-    name: 'christie',
-    email: 'christie@addu.edu.ph',
-  },
-  {
-    name: 'joe',
-    email: 'joe@addu.edu.ph',
-  },
-];
-
-const registeredStudents: StudentType[] = [
-  {
-    name: 'Alice',
-    uuid: '123',
-    yearCourse: 'Computer Science',
-  },
-  {
-    name: 'Bob',
-    uuid: '456',
-    yearCourse: 'Engineering',
-  },
-];
-
-const Button: FC<ButtonProps> = ({ text, onClickHandler }) => {
-  return (
-    <button
-      className="bg-white text-black px-3 py-2 rounded-lg"
-      onClick={onClickHandler}
-    >
-      {text}
-    </button>
-  );
-};
-
-const SearchBar = () => {
-  return (
-    <div className="flex items-center space-x-2">
-      <input type="text" placeholder="Search" />
-      <Button text="Search" />
-    </div>
-  );
-};
-
-const page = ({ params }: { params: { id: string } }) => {
-  const handleViewAcceptedPayments = () => {
-    console.log('View Accepted Payments button clicked!');
-  };
-
-  const handleViewDeclinedPayments = () => {
-    console.log('View Declined Payments button clicked!');
-  };
+const EventsPage = async ({ params }: { params: { id: string } }) => {
+  const eventData = await getEventData(params.id);
+  const acceptedPayments = await getPaymentsData(params.id, 'accepted');
+  const declinedPayments = await getPaymentsData(params.id, 'declined');
 
   return (
     <div className="container mx-auto">
-      <div className="w-1/2 my-10">
-        <h1 className="text-3xl">Event Name</h1>
-        <p className="mb-3 text-xl">Event Price</p>
-        <p>
-          Sit aute sit amet mollit laboris anim deserunt enim eiusmod est id
-          ullamco. Eu aliquip enim id in tempor dolor enim nisi id exercitation
-          laborum.
-        </p>
+      <div className="w-full lg:w-1/2 my-10">
+        <h1 className="text-3xl">{eventData.title}</h1>
+        <p className="mb-3 text-xl">{eventData.price}</p>
+        <p>{eventData.description}</p>
 
-        <div className="my-3 space-y-2 space-x-2">
-          <Link href={`/events/${params.id}/register`}>
+        <div className="my-3 flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3">
+          <Link href={`/events/${eventData.form_name}/register`}>
             <Button text="View Form" />
           </Link>
-          <Button
-            text="View Accepted Payments"
-            onClickHandler={handleViewAcceptedPayments}
-          />
-          <Button
-            text="View Declined Payments"
-            onClickHandler={handleViewDeclinedPayments}
-          />
+          <PaymentsModal type={'accepted'} listOfPayments={acceptedPayments} />
+          <PaymentsModal type={'declined'} listOfPayments={declinedPayments} />
         </div>
       </div>
 
       <div className="my-10">
-        <div className="flex justify-between">
-          <p className="text-2xl">Facilitators</p>
-          <SearchBar />
-        </div>
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th>{''}</th>
-              <th>Name</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {facilitators.map((facilitator: FacilitatorType, index: number) => (
-              <tr
-                key={index}
-                className="text-center even:bg-slate-600 odd:bg-slate-400"
-              >
-                <td>
-                  <input type="checkbox" />
-                </td>
-                <td>{facilitator.name}</td>
-                <td>{facilitator.email}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <div className="my-10">
-        <div className="flex justify-between">
-          <p className="text-2xl">Registered Students</p>
-          <SearchBar />
-        </div>
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th>{''}</th>
-              <th>UUID</th>
-              <th>Name</th>
-              <th>Year & Course</th>
-            </tr>
-          </thead>
-          <tbody>
-            {registeredStudents.map((student: StudentType) => (
-              <tr
-                key={student.uuid}
-                className="text-center even:bg-slate-600 odd:bg-slate-400"
-              >
-                <td>
-                  <input type="checkbox" />
-                </td>
-                <td>{student.uuid}</td>
-                <td>{student.name}</td>
-                <td>{student.yearCourse}</td>
-                <td>
-                  <button>View</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <StudentsTable.default list={eventData.students} />
       </div>
     </div>
   );
 };
 
-export default page;
+export default EventsPage;
