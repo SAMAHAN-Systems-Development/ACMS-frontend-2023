489345e3a3f9a078751992e8de1528bb2062f2b7
Viver Bungag <viverbungag1@gmail.com>
2024-02-11 13:51:08 +0800

    not fully functional commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import FacilitatorHomePage from '@/components/home/FacilitatorHomePage';
 import { useUser } from '@/contexts/UserContext';
 
 const Home = () => {
   const { user }: any = useUser();
+
+  if (user.userType === 'facilitator') {
+    return <FacilitatorHomePage />;
+  }
+
   return (
     <form>
       <p>{user?.userType}</p>
---
+++ src/utilities/fetch/event.ts
@@ -0,0 +1,20 @@
+const backendUrl =
+  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
+
+export const fetchActiveEvents = async (token: string, page: number) => {
+  const response = await fetch(`${backendUrl}/events/active?page=${page}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Error in fetching the active events');
+  }
+
+  const responseData = await response.json();
+  const { acceptedPayments, maxPage } = responseData;
+
+  return { payments: acceptedPayments, maxPage };
+};
