2d422c4cdee61b82f463828338b88dce7ae219c9
ryanlozano07 <ryan.rjl38@gmail.com>
2024-02-18 02:24:24 +0800

    using MUI for toggle component, completed!

---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { ToggleComponent } from '@/components/toggle/ToggleComponent'
+
+
+const page = () => {
+    return (
+        <div className='p-4'>
+             <ToggleComponent />
+        </div>
+
+    )
+    }
+
+export default page
---
+++ src/components/toggle/ToggleComponent.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Switch, { SwitchProps } from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { styled } from '@mui/material/styles';
+
+
+
+export const ToggleComponent:React.FC = () => {
+    return (
+        <FormGroup>
+          <FormControlLabel
+            control={<IOSSwitch sx={{ m: 1 }} defaultChecked  />}
+            label=""
+          />
+        </FormGroup>
+      );
+}
+
+const IOSSwitch = styled((props: SwitchProps) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+  ))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+      padding: 0,
+      margin: 2,
+      transitionDuration: '300ms',
+      '&.Mui-checked': {
+        transform: 'translateX(16px)',
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          backgroundColor: theme.palette.mode === 'dark' ? '#181842' : '#7A92C2',
+          opacity: 1,
+          border: 0,
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+          opacity: 0.5,
+        },
+      },
+      '&.Mui-focusVisible .MuiSwitch-thumb': {
+        color: '#33cf4d',
+        border: '6px solid #fff',
+      },
+      '&.Mui-disabled .MuiSwitch-thumb': {
+        color:
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[600],
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxSizing: 'border-box',
+      width: 22,
+      height: 22,
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: 26 / 2,
+      backgroundColor: theme.palette.mode === 'light' ? '#181842' : '#7A92C2',
+      opacity: 1,
+      transition: theme.transitions.create(['background-color'], {
+        duration: 500,
+      }),
+    },
+  }));
+  
