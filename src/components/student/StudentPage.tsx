a68e21ad65719edcfa51b0f25af83ac99fd08e03
unknown <paololuisramirez@gmail.com>
2024-02-18 15:47:21 +0800

    feat: removed routerdom, changed to next/navigation

--- src/components/student/StudentPage.tsx
+++ src/components/student/StudentPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'next/navigation';
 
 import type Student from '@/types/Student';
 
