88ee4355f9a347513ae15f084accc97a92b03447
Faahm <fatmahinay99@gmail.com>
2024-02-19 19:08:56 +0800

    Restyle InputFile to match Figma UI

--- src/components/ui/InputFile.tsx
+++ src/components/ui/InputFile.tsx
@@ -10,10 +10,10 @@ const InputFile = () => {
   };
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center mt-4">
       <label
         htmlFor="file-input"
-        className="px-1.5 py-2 font-bold text-[#181842] border-2 border-r-0 border-[#181842] cursor-pointer"
+        className="px-1.5 py-2 font-bold text-[#181842] border-2 border-r-0 border-[#181842] cursor-pointer whitespace-nowrap"
         style={{ backgroundColor: '#C4C4C4' }}
       >
         <input
@@ -26,6 +26,7 @@ const InputFile = () => {
         Choose File
       </label>
       <TextField
+        fullWidth
         size="small"
         value={selectedFile ? selectedFile.name : ''}
         InputProps={{
