6c13200e59508bb5f3c6d962d4f96c45a5b7b2cf
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2024-02-25 17:26:08 +0800

    WIP

--- src/components/add-event/components/AddEventNavBar.tsx
+++ src/components/add-event/components/AddEventNavBar.tsx
@@ -1,8 +1,19 @@
+import AddEventNavbarButton from '@/components/add-event/components/AddEventNavbarButton'
 import React from 'react'
 
 const AddEventNavBar = () => {
     return (
-        <div>AddEventNavbar</div>
+        <div className='flex justify-between w-[100%]'>
+            <div>
+                ACMS
+            </div>
+
+            <div className='flex gap-5'>
+                <AddEventNavbarButton text={'Home'} />
+                <AddEventNavbarButton text={'Events'} />
+                <AddEventNavbarButton text={'Logout'} />
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/add-event/components/AddEventNavbarButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddEventNavbarButton = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default AddEventNavbarButton
