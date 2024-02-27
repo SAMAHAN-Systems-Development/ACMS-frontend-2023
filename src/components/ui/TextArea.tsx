d8e4d49d356c4f3504b09473bf79fcdb58a1b0f8
Viver Bungag <viverbungag1@gmail.com>
2024-02-28 00:24:08 +0800

    chore: updated the text area component to match the text field component

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,45 +1,45 @@
 import * as React from 'react';
 
-import {TextField as BaseTextField} from '@mui/material';
+import { TextField as BaseTextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const TextArea = styled(BaseTextField)({
-  "& label.Mui-focused": {
+  '& label.Mui-focused': {
     color: '#2B2B2B',
-    fontWeight: 'bold',
     fontFamily: 'Work Sans, sans-serif',
   },
-  "& .MuiOutlinedInput-root": {
-    borderRadius: '11px',
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '5px',
     minHeight: '120px',
     display: 'flex',
     alignItems: 'flex-start',
-    "&.Mui-focused fieldset": {
+    '&.Mui-focused fieldset': {
       boxSizing: 'border-box',
       borderColor: '#181842',
       borderWidth: '2px',
-    }
+    },
   },
-  "& input": {
-    fontWeight: 'bold',
+  '& fieldset': {
+    borderColor: '#181842',
+    borderWidth: '2px',
+    color: '#181842',
+  },
+  '& input': {
     fontFamily: 'Work Sans, sans-serif',
-  }
+    color: '#181842',
+  },
 });
 
 type TextAreaProps = {
-  className: string,
-  id: string,
-  label: string
+  className: string;
+  id: string;
+  label: string;
 };
 
-export default function CustomizedInputsStyled({ label, id, className }: TextAreaProps) {
-  return (
-      <TextArea
-        id={id}
-        label={label}
-        className={className}
-        focused
-        multiline
-      />
-  );
+export default function CustomizedInputsStyled({
+  label,
+  id,
+  className,
+}: TextAreaProps) {
+  return <TextArea id={id} label={label} className={className} multiline />;
 }
