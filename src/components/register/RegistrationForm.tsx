0984e38b68ae5bca95399c1dcc61bf20d4790e35
Rhaey <rhaeyislyfe@gmail.com>
2024-09-26 01:18:29 +0800

    feat: update gcash details

--- src/components/register/RegistrationForm.tsx
+++ src/components/register/RegistrationForm.tsx
@@ -334,8 +334,8 @@ const RegistrationForm = ({ formName }: { formName: string }) => {
                   <div className="flex flex-col gap-2 w-full">
                     <div>
                       <h3 className="font-bold">GCash</h3>
-                      <p>CHRISTOPHER RAY SUAYBAGUIO ARAGON</p>
-                      <p>0968 328 2851</p>
+                      <p>Gabriel Paolo G. Abucayon</p>
+                      <p>0991 153 7348</p>
                     </div>
                     <div>
                       <h3 className="font-bold">BPI</h3>
